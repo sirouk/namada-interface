e6808085296a917f2610ccfac02e216e4d29702b
Justin R. Evans <jurevans@gmail.com>
2024-09-30 08:56:13 -0400

    feat: update package name, regen sdk docs

--- apps/namadillo/src/hooks/useSdk.tsx
+++ apps/namadillo/src/hooks/useSdk.tsx
@@ -1,5 +1,5 @@
-import initSdk from "@heliax/namada-sdk/inline-init";
-import { getSdk, Sdk } from "@heliax/namada-sdk/web";
+import initSdk from "@heliaxdev/namada-sdk/inline-init";
+import { getSdk, Sdk } from "@heliaxdev/namada-sdk/web";
 import { nativeTokenAddressAtom } from "atoms/chain";
 import { rpcUrlAtom } from "atoms/settings";
 import { getDefaultStore, useAtomValue } from "jotai";
